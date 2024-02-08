e6d94887f561e48709a455917dbef3cb8b7b509d
xaldovah <dxaldovah@gmail.com>
2024-02-08 16:49:30 +0300

    function called filter_datum that returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,23 +3,31 @@
 This module function returns the log message obfuscated
 """
 
+from typing import List
 import re
 
+rgx = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+data = ("name", "email", "phone", "ssn", "password")
 
-def filter_datum(fields, redaction, message, separator):
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
     """
     Obfuscate specified fields in a log message.
 
     Arguments:
-    fields: list of strings representing fields to obfuscate
-    redaction: string representing the replacement for obfuscated fields
-    message: string representing the log line
-    separator: string representing the character separating fields
-    in the log line
+        fields: list of strings representing fields to obfuscate
+        redaction: string representing the replacement for obfuscated fields
+        message: string representing the log line
+        separator: string representing the character separating fields
+        in the log line
 
     Returns:
-    String: Log message with specified fields obfuscated.
+        String: Log message with specified fields obfuscated.
     """
-    pattern = re.compile(r'({}=)([^;]+)'.format('|'.join(fields), re.escape(
-        separator)))
-    return pattern.sub(r'\1' + redaction, message)
+    extract, replace = (rgx["extract"], rgx["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
