8968cb8eb1d26368b5593a63a159ee9f4f415acb
xaldovah <dxaldovah@gmail.com>
2024-02-08 18:14:04 +0300

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,11 +3,13 @@
 This module function returns the log message obfuscated
 """
 
+import os
 from typing import List
 import logging
 from logging import StreamHandler
 import re
 import csv
+import mysql.connector
 
 
 rgx = {
@@ -57,34 +59,47 @@ def filter_datum(
     return re.sub(extract(fields, separator), replace(redaction), message)
 
 
+def get_db():
+    """
+    Returns a connector to the database.
+
+    Returns:
+    mysql.connector.connection.MySQLConnection: Connector to the database.
+    """
+    # Get database credentials from environment variables
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    # Connect to the database
+    try:
+        connection = mysql.connector.connect(
+            user=username,
+            password=password,
+            host=host,
+            database=db_name
+        )
+        print("Connected to the database successfully!")
+        return connection
+    except mysql.connector.Error as err:
+        print("Error:", err)
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
     SEPARATOR = ";"
 
-    def __init__(self, fields):
-        """
-        Initialize RedactingFormatter with a list of fields to redact.
-
-        Args:
-            fields (list): List of strings representing fields to redact.
-        """
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """formats a LogRecords
         """
-        Format the log record, filtering values for specified fields.
-        Args:
-            record (logging.LogRecord): The log record to format.
-        Returns:
-            str: Formatted log message.
-        """
-        message = record.getMessage()
-        for field in self.fields:
-            message = filter_datum(
-                [field], self.REDACTION, message, self.SEPARATOR)
-        record.msg = message
-        return super().format(record)
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
